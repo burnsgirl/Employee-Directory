508111dd0637e43b6eac5dfacf776d2331f26eb3
Dauphine Burns <dauphineburns@gmail.com>
2021-09-07 09:40:11 -0400

    removed wrapper for meantime

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -2,15 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './components/title';
 import EmployeeCard from './components/cards';
-import Wrapper from './Wrapper';
+// import Wrapper from './components/Wrapper';
 
 function App() {
   return (
-    <Wrapper>
+    <body>
       <Title />
       <EmployeeCard />
 
-    </Wrapper>
+    </body>
 
   );
 }
--- employeedirectory/src/components/Wrapper/index.js
+++ employeedirectory/src/components/Wrapper/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 
-function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+function Wrapper() {
+  return <div></div>;
 }
 
 export default Wrapper;
