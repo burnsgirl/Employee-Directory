dd94c54e0bbf4d9a56a707b3215f50a742a7c0ce
Dauphine Burns <dauphineburns@gmail.com>
2021-09-07 09:36:16 -0400

    imported wrapper on app.js

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Title from './components/title';
 import EmployeeCard from './components/cards';
+import Wrapper from './Wrapper';
 
 function App() {
   return (
