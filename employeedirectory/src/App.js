65316a987b6949b7c73ba35a04b92bb546d22d6f
Dauphine Burns <dauphineburns@gmail.com>
2021-09-07 11:28:13 -0400

    delted unused files

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,9 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Title from './components/title';
-// import EmployeeCard from './components/cards';
 import Employee from './components/generator';
-// import Wrapper from './components/Wrapper';
 
 function App() {
   return (
